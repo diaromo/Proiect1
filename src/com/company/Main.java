999d2c0af5ef8643909e3e52654b8d8cce8764d2
“Diana <romosandiana@gmail.com>
2020-12-09 21:29:06 +0200

    Diana

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,5 +10,42 @@ public class Main {
         System.out.println(5+3);
         System.out.println(Integer.sum(5,3));
         System.out.println("Sum of 5 and 3 is " + Integer.sum(5,3));
+        System.out.println("Sum of 5 and 3 is " + (5+3));
+
+        int a = 10;
+        double b = 3;
+        System.out.println(a/b);
+
+        int a1 = 10;
+        int b1 = 3;
+        double c1=b1;
+        System.out.println(a1/c1);
+
+        System.out.println(-5+8*6);
+
+        int x = -5;
+        int y = 8;
+        int z = 6;
+        System.out.println(x+y*z);
+
+        System.out.println((55+9)%9);
+
+
+       double z1 = 20;
+       double z2 = -3;
+       double z3 = 5;
+       double z4 = 8;
+       System.out.println(z1+z2*z3/z4);
+       System.out.println(20 + -3*5 / 8);
+
+
+       System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+
+
+
+
+
+
+
     }
 }
