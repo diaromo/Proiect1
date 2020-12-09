35970f68b0bc8ac4bf35794497a46d6db2590fa9
“Diana <romosandiana@gmail.com>
2020-12-09 19:44:22 +0200

    Diana

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,9 @@ public class Main {
 	// write your code here
         System.out.println("Hello");
         System.out.println("Diana");
+
+        System.out.println(5+3);
+        System.out.println(Integer.sum(5,3));
+        System.out.println("Sum of 5 and 3 is " + Integer.sum(5,3));
     }
 }
