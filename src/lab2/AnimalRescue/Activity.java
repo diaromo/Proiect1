c7fc8effbd7aeb76dbf2fc8599fa89e267b334c0
“Diana <romosandiana@gmail.com>
2020-12-13 18:12:51 +0200

    Tema Obiecte

---
+++ src/lab2/AnimalRescue/Activity.java
@@ -0,0 +1,10 @@
+package lab2.AnimalRescue;
+
+public class Activity {
+
+    String duration = "60 " + "Minutes";
+    int durationMinutes = 60;
+    String location = "indoor"; //outside
+
+
+}
---
+++ src/lab2/AnimalRescue/Adopter.java
@@ -0,0 +1,10 @@
+package lab2.AnimalRescue;
+
+public class Adopter {
+
+    String name = "Diana";
+    double budget = 1000;
+    String address = "Zorilor";
+    char sex = 'F';
+
+}
