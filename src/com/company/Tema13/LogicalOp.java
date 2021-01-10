9dbb20ba3e75f40c4c0b5be9e58c47f427ff922f
“Diana <romosandiana@gmail.com>
2021-01-10 16:07:40 +0200

    Arrays

--- src/com/company/Tema13/LogicalOp.java
+++ src/com/company/Tema13/LogicalOp.java
@@ -24,12 +24,103 @@ public class LogicalOp {
     }
 
     //Java Arrays Punct 4
-    public double getAverageFromArray(int[] array){
+    public double getAverageFromArray(int[] array) {
         double sum = 0;
-        for (int i = 0; i < array.length; i++){
+        for (int i = 0; i < array.length; i++) {
             sum = sum + array[i];
         }
-        return sum/array.length;
+        return sum / array.length;
     }
+
+    //Java Arrays Punct 5
+
+    public boolean checkValueFromString(String[] a, String s){
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == s) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Java Arrays Punct 6
+
+
+    public int checkPositionFromArray(int[] a, int s){
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == s) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //Java Arrays Punct 7
+
+    public void printSymbols(String[] s){
+        for (int i=0; i < 10; i++){
+            for(int j=0; j < s.length; j++){
+                System.out.print(s[j]);
+            }
+            System.out.print("\n");
+        }
+    }
+
+    //Java Arrays Punct 8
+
+    public int[] checkArray(int[] a, int s){
+        int[] b = new int[a.length-1];
+
+        for (int i = 0, j = 0; i < a.length; i++) {
+            if (a[i] != s) {
+                b[j++] = a[i];
+            }
+        }
+        return b;
+    }
+
+    //Java Arrays Punct 9
+
+
+    public int returnSeconSmallest (int [] a){
+        int smallest = a[0];
+        int secondSmallest = a[0];
+        for (int i = 0; i < a.length; i++){
+            if (a[i] < smallest){
+                secondSmallest = smallest;
+                smallest = a[i];
+            }
+            else if (a[i] < secondSmallest) {
+                secondSmallest = a[i];
+            }
+        }
+        return secondSmallest;
+    }
+
+    //Java Arrays Punct 10
+
+    public int[] copyArray(int[] a, int[] b){
+
+        for (int i = 0; i < a.length; i++) {
+           b[i]=a[i];
+        }
+        return b;
+    }
+    //Arrays Optional Punct 2
+
+    public String returnBigAndSmall (int [] a){
+        int smallest = a[0];
+        int biggest = a[0];
+        for (int i = 0; i < a.length; i++){
+            if (a[i] < smallest){
+                smallest = a[i];
+            }
+            if (a[i] > biggest) {
+                biggest = a[i];
+            }
+        }
+        return ("\nSmallest =" + smallest + " and Biggest = " + biggest);
+    }
+
 }
 
--- src/com/company/Tema13/Main.java
+++ src/com/company/Tema13/Main.java
@@ -39,5 +39,50 @@ public class Main {
         //Java Arrays Punct 4
         System.out.println("\n " + logic.getAverageFromArray(myFirstArray));
         System.out.println("\n " + logic.getAverageFromArray(arrayGol));
+
+        //Java Arrays Punct 5
+        String[] myStringArray = {"Didi","Petru","Cat"};
+        System.out.println(logic.checkValueFromString(myStringArray, "Dog"));
+        System.out.println(logic.checkValueFromString(myStringArray, "Cat"));
+        System.out.println(logic.checkValueFromString(myStringArray, "cat"));
+
+        //Java Arrays Punct 6
+        int[] myNumberArray = {5,8,20,26,27};
+        System.out.println(logic.checkPositionFromArray(myNumberArray,26));
+        System.out.println(logic.checkPositionFromArray(myNumberArray,0));
+        System.out.println(logic.checkPositionFromArray(myNumberArray,5));
+
+        //Java Arrays Punct 7
+        String[] symbolArray = {"-","-","-","-","-","-","-","-","-","-"};
+        logic.printSymbols(symbolArray);
+
+        //Java Arrays Punct 8
+        int[] printArray = {5,8,20,26,27};
+        for (int x=0; x <printArray.length; x++ ){
+            System.out.print(printArray[x] + " ");
+        }
+        System.out.print("\n");
+        int[] printArray2 = logic.checkArray(printArray,26);
+        for (int x=0; x <printArray2.length; x++ ){
+            System.out.print(printArray2[x] + " ");
+        }
+
+        //Java Arrays Punct 9
+        int [] printSmallest = {7,3,4,1,2,-1,0,11,10};
+        System.out.print("\n");
+        System.out.println(logic.returnSeconSmallest(printSmallest));
+
+        //Java Arrays Punct 10
+        int[] copyArray1 = {2,6,-1,4};
+        int[] copyArray2 = new int[4];
+        copyArray2 = logic.copyArray(copyArray1, copyArray2);
+        for (int z = 0; z < copyArray2.length; z++) {
+            System.out.print(copyArray2[z] + " ");
+
+        }
+
+        //Java Arrays Optional Punct 2
+        int[] numbers = {2,6,-1,4};
+        System.out.println(logic.returnBigAndSmall(numbers));
     }
 }
